ad21f5ab74b5b49f3b2f5b197faf78cb95b96383
sdhornet <sdhornet1@gmail.com>
2026-09-13 20:48:33 -0500

    added the nosniff header middleware function

--- internal/httpserver/middleware.go
+++ internal/httpserver/middleware.go
@@ -59,6 +59,13 @@ func cspNonce(next http.Handler) http.Handler {
 	})
 }
 
+func noSniffHeader(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Add("X-Content-Type-Options", "nosniff")
+		next.ServeHTTP(responseWriter, request)
+	})
+}
+
 func recoverPanics(logger *logging.Logger, renderer *templates.Renderer) middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
